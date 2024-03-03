9cd3a1a6cc2420eb11e7f9af5aa719dbfabdb01e
unknown <oshni.sharma@antcreatives.com>
2024-03-04 01:28:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import scissors from "./images/scissors.png";
+import paper from "./images/paper.png";
+import rock from "./images/rock.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+export const Player = ({ weapon, playerName }) => (
+  <div className="player-main-container">
+    <h2>{playerName}</h2>
+    <div className="player">
+      <img
+        className="player-image"
+        src={
+          weapon === "rock" ? rock : weapon === "scissors" ? scissors : paper
+        }
+        alt="Rock Paper Scissors"
+      />
     </div>
-  );
-}
+  </div>
+);
 
-export default App;
+export const PlayerTwo = ({ weapon, playerName }) => (
+  <div className="player-main-container">
+    <h2>{playerName}</h2>
+    <div className="player">
+      <img
+        className="player-image2"
+        src={
+          weapon === "rock" ? rock : weapon === "scissors" ? scissors : paper
+        }
+        alt="Rock Paper Scissors"
+      />
+    </div>
+  </div>
+);
+
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,163 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
+import { Player, PlayerTwo } from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const weapons = ["rock", "paper", "scissors"];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+  const [playerOne, setPlayerOne] = useState(weapons[0]);
+  const [playerTwo, setPlayerTwo] = useState(weapons[0]);
+  const [winner, setWinner] = useState("");
+  const [playerOneScore, setPlayerOneScore] = useState(0);
+  const [playerTwoScore, setPlayerTwoScore] = useState(0);
+  const [overallWinner, setOverallWinner] = useState(null);
+  const [round, setRound] = useState(0);
+  const [playerOneName, setPlayerOneName] = useState("Player One");
+  const [playerTwoName, setPlayerTwoName] = useState("Player Two");
+  const [totalRounds, setTotalRounds] = useState(5);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    if (name === "totalRounds") {
+      setRound(0);
+      setPlayerOneScore(0);
+      setPlayerTwoScore(0);
+      setOverallWinner(null);
+    }
+    if (name === "playerOneName") {
+      setPlayerOneName(value);
+    }
+    if (name === "playerTwoName") {
+      setPlayerTwoName(value);
+    }
+    if (name === "totalRounds") {
+      setTotalRounds(value);
+    }
+  };
+
+  const startGame = () => {
+    if (round === parseInt(totalRounds)) {
+      resetGame();
+      return;
+    }
+
+    let counter = 0;
+    let gameInterval = setInterval(() => {
+      counter++;
+      setPlayerTwo(weapons[Math.floor(Math.random() * weapons.length)]);
+      setWinner("");
+      if (counter > 5) {
+        clearInterval(gameInterval);
+        let overallWinner = null;
+        if (playerOneScore > 2) {
+          overallWinner = playerOneName;
+        } else if (playerTwoScore > 2) {
+          overallWinner = playerTwoName;
+        }
+        setWinner(selectWinner());
+        setOverallWinner(overallWinner);
+        setRound((prevRound) => prevRound + 1);
+      }
+    }, 100);
+  };
+
+  const selectWinner = () => {
+    let winner = "";
+
+    if (playerOne === playerTwo) {
+      winner = "It's a Tie Try Again!";
+    } else if (
+      (playerOne === "rock" && playerTwo === "scissors") ||
+      (playerOne === "scissors" && playerTwo === "paper") ||
+      (playerOne === "paper" && playerTwo === "rock")
+    ) {
+      winner = playerOneName;
+      setPlayerOneScore((prevScore) => prevScore + 1);
+    } else {
+      winner = playerTwoName;
+      setPlayerTwoScore((prevScore) => prevScore + 1);
+    }
+
+    return winner;
+  };
+
+  const resetGame = () => {
+    setPlayerOneScore(0);
+    setPlayerTwoScore(0);
+    setOverallWinner(null);
+    setRound(0);
+  };
+
+  useEffect(() => {
+    if (round === parseInt(totalRounds)) {
+      resetGame();
+    }
+  }, [round, totalRounds]);
+
+  return (
+    <>
+      <h1 style={{ textAlign: "center", textTransform:"uppercase" ,fontSize:"35px"}}>Rock Paper Scissors</h1>
+      <div className="container">
+        <input
+          type="text"
+          placeholder="Enter Player One Name"
+          name="playerOneName"
+          value={playerOneName}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Enter Player Two Name"
+          name="playerTwoName"
+          value={playerTwoName}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          placeholder="Enter Total Rounds"
+          name="totalRounds"
+          value={totalRounds}
+          onChange={handleChange}
+        />
+        <h1>
+          {playerOneName} Score: {playerOneScore}
+        </h1>
+        <h1>
+          {playerTwoName} Score: {playerTwoScore}
+        </h1>
+        <h2>
+          Round: {round}/{totalRounds}
+        </h2>
+        <div className="player-container">
+          <Player weapon={playerOne} playerName={playerOneName} />
+          <PlayerTwo weapon={playerTwo} playerName={playerTwoName} />
+        </div>
+        <div className="button-container">
+          <button className="weaponBtn" onClick={() => setPlayerOne("rock")}>
+            Rock
+          </button>
+          <button className="weaponBtn" onClick={() => setPlayerOne("paper")}>
+            Paper
+          </button>
+          <button
+            className="weaponBtn"
+            onClick={() => setPlayerOne("scissors")}
+          >
+            Scissors
+          </button>
+        </div>
+        <div className="winner">
+          {winner ? `${winner} wins!` : null}
+          {overallWinner ? `Overall winner: ${overallWinner}` : null}
+        </div>
+        <button type="button" onClick={startGame}>
+          {round === parseInt(totalRounds) ? "Reset Game" : "Start Game"}
+        </button>
+      </div>
+    </>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
